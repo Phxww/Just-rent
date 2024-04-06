4c97003e768af568500a9312caa8af1fbcfc1b3c
Anna0118 <annachu0988@gmail.com>
2024-04-06 22:21:49 +0800

    add static and home page

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,18 @@ from app import app
 from flask import render_template
 
 
+@app.route('/')
+def home():
+    # 撈出所有cars的資料
+    return render_template('index.html')
+
 @app.route('/cars')
 def cars():
+    # 撈出所有cars的資料
     return render_template('cars.html')
+
+
+# @app.route('/cars/:car_id')
+# def cars():
+#     # 撈出單一汽車規格的資訊
+#     return render_template('car-single.html')
