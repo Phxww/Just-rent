0ddade24dd7fb0b3948bb75e6703b52cd3eb949c
Anna0118 <annachu0988@gmail.com>
2024-04-08 16:54:26 +0800

    add cars table and crawler script

--- app/models.py
+++ app/models.py
@@ -12,4 +12,15 @@ class User(db.Model):
 class Car(db.Model):
     __tablename__ = 'cars'
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False, default='Unknown')
+    brand = db.Column(db.String(255))
+    year = db.Column(db.String(255), nullable=False, default='Unknown')
+    model = db.Column(db.String(255))
+    door = db.Column(db.Integer, nullable=False, default=0)
+    body = db.Column(db.String(255), nullable=False, default='Unknown')
+    displacement = db.Column(db.String(255), nullable=False, default='Unknown')
+    seat = db.Column(db.Integer, nullable=False, default=0)
+    car_length = db.Column(db.String(255), nullable=False, default='Unknown')
+    wheelbase = db.Column(db.String(255), nullable=False, default='Unknown')
+    power_type = db.Column(db.String(255), nullable=False, default='Unknown')
     
