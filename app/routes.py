fda0101f8c701c08c3989bff5b7e54597180efdc
Anna0118 <annachu0988@gmail.com>
2024-04-08 18:38:08 +0800

    add cars api and use Postman test

--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,33 @@
 from app import app
-from flask import render_template
-from app.models import User
+from flask import render_template, request, jsonify
+from app.models import User, Car
 
 
 @app.route('/')
 def home():
-    # 撈出所有cars的資料
     return render_template('index.html')
 
-@app.route('/cars')
+
+@app.route('/api/cars')
 def cars():
-    # 撈出所有cars的資料
-    return render_template('cars.html')
+    brand = request.args.get('brand')
+    if brand:
+        # 從資料庫撈出指定的廠牌
+        cars = Car.query.filter_by(brand=brand).all()
+    else:
+        # 如果沒有指定廠牌，就撈出所有資料
+        cars = Car.query.all()
+
+    # 以字典返回資料
+    cars_list = [{
+        'name': car.name,
+        'brand': car.brand,
+        'year': car.year,
+        'model': car.model
+    } for car in cars]
+
+    # 以 JSON 格式回傳
+    return jsonify(cars_list)
 
 
 # @app.route('/cars/:car_id')
